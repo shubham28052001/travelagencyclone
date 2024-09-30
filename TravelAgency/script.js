38ef219b1c8145c21a0f61c5f5e1a48e74ad5db4
shubham <shubhamsanodiya2805@gmail.com>
2024-10-01 01:02:36 +0530

    this ia travel palnner website

---
+++ TravelAgency/script.js
@@ -0,0 +1,19 @@
+
+const overlay = document.querySelector("overlay");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i = 200;i++) {
+    if(true){
+        header.classList.add("active");
+    goTopBtn.classList.add("active");
+    }
+  }
+   
+
+}
